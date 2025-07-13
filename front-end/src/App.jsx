3b4d9e9b202c0fefd0946ec2016a615df22c1e42
Diego Amorim <diego@hashtagtreinamentos.com>
2025-07-13 20:13:40 -0300

    mudei a baseURL do axios

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,7 +7,10 @@ import Carrinho from "./components/Carrinho";
 import { createContext, useState } from "react";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://imersao-javascript-na-pratica-jfw4.onrender.com/";
 
 export const ContextoCarrinho = createContext(null);
 
